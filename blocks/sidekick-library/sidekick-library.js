2b33c0cbae13a642600cda2b1c0df644c7acbc6b
Dylan Depass <dylandepass@gmail.com>
2023-04-14 12:46:27 -0400

    feat: add sidekick library block

---
+++ blocks/sidekick-library/sidekick-library.js
@@ -0,0 +1,14 @@
+import '../../library/library.js';
+import { readBlockConfig } from '../../scripts/lib-franklin.js';
+
+export default function decorate(block) {
+  const blockConfig = readBlockConfig(block);
+  const library = document.createElement('franklin-library');
+  library.config = {};
+
+  Object.keys(blockConfig).forEach((attr) => {
+    library.config[attr] = blockConfig[attr];
+  })
+
+  block.replaceChildren(library);
+}
