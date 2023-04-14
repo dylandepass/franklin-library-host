d115042737910a98082d2fbd8b8b91cf92786236
Dylan Depass <dylandepass@gmail.com>
2023-04-14 12:57:52 -0400

    fix: update library, remove block config

--- blocks/sidekick-library/sidekick-library.js
+++ blocks/sidekick-library/sidekick-library.js
@@ -4,11 +4,6 @@ import { readBlockConfig } from '../../scripts/lib-franklin.js';
 export default function decorate(block) {
   const blockConfig = readBlockConfig(block);
   const library = document.createElement('franklin-library');
-  library.config = {};
-
-  Object.keys(blockConfig).forEach((attr) => {
-    library.config[attr] = blockConfig[attr];
-  })
 
   block.replaceChildren(library);
 }
