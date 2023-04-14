5443b9a5e60d061d04ac3d4f451365be50c68e44
Dylan Depass <dylandepass@gmail.com>
2023-04-14 12:40:16 -0400

    fix: update

--- library/utils/dom.js
+++ library/utils/dom.js
@@ -39,11 +39,11 @@ export function createCopy(blob) {
   navigator.clipboard.write(data);
 }
 
-export const getMetadata = (el) => [...el.childNodes].reduce((rdx, row) => {
+export const getMetadata = el => [...el.childNodes].reduce((rdx, row) => {
   if (row.children) {
     const key = row.children[0].textContent.trim().toLowerCase();
     const content = row.children[1];
-    const text = content.textContent.trim().toLowerCase();
+    const text = content?.textContent.trim().toLowerCase();
     if (key && content) rdx[key] = { content, text };
   }
   return rdx;
@@ -59,8 +59,8 @@ export function loadCSS(href, callback) {
     link.setAttribute('rel', 'stylesheet');
     link.setAttribute('href', href);
     if (typeof callback === 'function') {
-      link.onload = (e) => callback(e.type);
-      link.onerror = (e) => callback(e.type);
+      link.onload = e => callback(e.type);
+      link.onerror = e => callback(e.type);
     }
     document.head.appendChild(link);
   } else if (typeof callback === 'function') {
